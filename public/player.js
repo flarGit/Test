d3c4abfcd59d84f9d74a05520ce47c917673e633
flarGit <flar21@gmx.net>
2018-07-26 14:26:42 +0200

    Add files via upload

--- public/player.js
+++ public/player.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-
+	var name = "";
 
 	// WebSocket
 	var socket = io.connect();
-	document.getElementById("javaskriptfehler").style.display == "none"
-	
+	window.onload = function () {
+		document.getElementById("javaskriptfehler").style.display = "none"
+		document.getElementById("loadingsite").style.display = "none"
+	}
 //20180717 Nachricht von user to user
 	function sendenChatctoc(){
 		// Eingabefelder auslesen
-		//var name = $('#name').val();
-		var name = "hallo";
 		var text = $('#chattestctoc').val();
 		// Socket senden
 		socket.emit('chatctoc', {id: "1", name: name, text: text });
@@ -20,13 +20,21 @@ $(document).ready(function(){
 	socket.on('awchatctoc', function (data) {
 		var zeit = new Date(data.zeit);
 		$('#chatctoc').append(
-			'<img id="B' + data.name + '" alt="" height="100" src="B'+ data.name +'.jpg" width="100">' +
-			'<div id="chatctocin" name="chatctocin">'+ data.text + '</div>'	
+			//'<img id="B' + data.name + '" alt="" height="100" src="B'+ data.name +'.jpg" width="100">' +
+			'<div id="chatctocname" name="chatctocname">'+ data.name + '</div>' +
+			'<div id="chatctocin" name="chatctocin">'+ data.text + '</div>'
 		);
 	});	
-//20180717 ctoc	
+	
+	
+	function sendenkeyname(){
+		name = $('#playerNameeingabe').val();
+		socket.emit('chatctoc', {id: "1", name: name, text: name + " ist dem chat beigetreten" });
+		document.getElementById("myName").style.display = "none"
+		document.getElementById("mychat").style.display = ""
+	}
 	// bei einem Klick auf senden
 	$('#sendenctockey').click(sendenChatctoc);
-	
+	$('#sendenkeyname').click(sendenkeyname);
 
 });
