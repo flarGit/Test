7c04c123467425bac30f5ee1c2d39d38f40a5434
flarGit <flar21@gmx.net>
2018-07-30 19:53:20 +0200

    Add files via upload

--- public/player.js
+++ public/player.js
@@ -4,15 +4,19 @@ $(document).ready(function(){
 	// WebSocket
 	var socket = io.connect();
 	socket.on('connect', function () {
-		var myName = location.search.split('?id=')[1]
-		socket.emit('join', {name:myName});
+		var temp = location.search.split('?id=')[1];
+		var myName = temp.split('?key=')[0];
+		var key  = temp.split('?key=')[1];
+		name = myName;
+		socket.emit('join', {name:myName,key:key});
 	});
 	
 	window.onload = function () {
-		document.getElementById("javaskriptfehler").style.display = "none"
-		document.getElementById("loadingsite").style.display = "none"
-		document.getElementById("myName").style.display = "none"
-		document.getElementById("mychat").style.display = ""
+		document.getElementById("javaskriptfehler").style.display = "none";
+		document.getElementById("loadingsite").style.display = "none";
+		document.getElementById("myName").style.display = "none";
+		document.getElementById("mychat").style.display = "none";
+		
 	}
 //20180717 Nachricht von user to user
 	function sendenChatctoc(){
@@ -33,6 +37,37 @@ $(document).ready(function(){
 		);
 	});	
 	
+	socket.on('waitstart', function (data) {
+		document.getElementById("wait").style.display = "";
+		$('#waitme').append('Wilkommen '+data.name+'<br>Bitte warten sie noch bis alle anderen Teilnehmer ('+data.groupsize+') bereit sind:');
+		for (i = 0; i < data.groupsize; i++) { 
+			$('#waitother').append('<font color="#FF0000">Player '+ (i+1) +'</font><br>');
+		}
+		tickenforwaitONOFF = true;
+		tickenforwait();
+	});	
+	socket.on('rejoinstatus', function (data) {
+		$('#waitother').empty();
+		
+		for (i = 0; i < data.groupsize; i++) {
+			if(data.gruen > i){
+				$('#waitother').append('<font color="#00C000">Player '+ (i+1) +'</font><br>');
+			}else if(data.gelb + data.gruen > i){
+				$('#waitother').append('<font color="#DBA901">Player '+ (i+1) +'</font><br>');
+			}else{
+				$('#waitother').append('<font color="#FF0000">Player '+ (i+1) +'</font><br>');
+			}
+		}
+	});
+	socket.on('startchat', function (data) {
+		tickenforwaitONOFF = false;
+		document.getElementById("wait").style.display = "none";
+		document.getElementById("mychat").style.display = "";
+	});
+	
+	socket.on('logout', function (data) {
+		location.replace('/logout');
+	});
 	
 	function sendenkeyname(){
 		name = $('#playerNameeingabe').val();
@@ -40,8 +75,36 @@ $(document).ready(function(){
 		document.getElementById("myName").style.display = "none"
 		document.getElementById("mychat").style.display = ""
 	}
+	
+	function waitbereit(){
+		if(document.getElementById("waitbereit").value === "bereit"){
+			document.getElementById("waitbereit").value = "nicht mehr bereit";
+			socket.emit('joinstatusgruen',{name:name});
+			
+		}
+		else if(document.getElementById("waitbereit").value === "nicht mehr bereit"){
+			document.getElementById("waitbereit").value = "bereit";
+			socket.emit('joinstatusgelb',{name:name});
+		}
+		socket.emit('joinstatus',{name:name});
+	}
 	// bei einem Klick auf senden
 	$('#sendenctockey').click(sendenChatctoc);
 	$('#sendenkeyname').click(sendenkeyname);
+	$('#waitbereit').click(waitbereit);
 
+	
+	var timeinterval = 3000;
+	var tickenforwaitONOFF = false;
+	function tickenforwait(){	
+		if(tickenforwaitONOFF){
+			//console.log("Hallo Tick");
+			joinstatusstart();
+			window.setTimeout(tickenforwait, timeinterval);
+		}
+	}
+	
+	function joinstatusstart(){
+		socket.emit('joinstatus',{name:name});
+	}
 });
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var debug = true;
 
+var groupsize = 2;
+var idcheck = new Map();
+var nametoGroup = new Map();
+var groupGelb = new Map();
+var groupGruen = new Map();
+var grouptoActive = new Map();		// 0 = start 1=warten 2 = läuft 3 = timeout
+
 var express = require('express');
 var app = express();
 
 var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
-
 
 app.use(cookieSession({
     name: 'session',
@@ -34,11 +40,14 @@ app.post('/login', function (req, res) {
     var user = req.body.username,
     pw = req.body.password;
     if (user === 'u1' && pw === 'test') {
+		nametoGroup.set(user,"G1");
         req.session.user = 'u1';
     } else if (user === 'u2' && pw === 'test') {
-        req.session.user = 'u2';
+		nametoGroup.set(user,"G1");
+		req.session.user = 'u2';
     }
-    res.redirect('/player'+'?id='+user);
+	idcheck.set(user,rand(100000,999999))
+    res.redirect('/player'+'?id='+user+'?key='+idcheck.get(user));
 });
 
 app.get('/player', checkAuth, function (req, res) {
@@ -47,17 +56,18 @@ app.get('/player', checkAuth, function (req, res) {
 	res.sendfile(__dirname + '/public/player.html', {user: req.session.user});
 });
 
+//kann eigendlich weg echzeit überwachung :)
 app.get('/logout', function (req, res) {
-    var names = Object.keys(connections);
+    var names = Object.keys(connectionsfull);
 
     names.splice(names.indexOf(req.session.user), 1);
 
     var msg = '{"names": ["' + names.join('","') + '"]}';
 
-    connections[req.session.user].broadcast.emit('join', msg);
+    connectionsfull[req.session.user].broadcast.emit('join', msg);
 
-    connections[req.session.user].disconnect();
-    delete connections[req.session.user];
+    connectionsfull[req.session.user].disconnect();
+    delete connectionsfull[req.session.user];
 
     delete req.session.user;
 
@@ -65,6 +75,11 @@ app.get('/logout', function (req, res) {
 	console.log('tschussssssss');
 });
 
+//funktionen----------------------------------------------------------------
+function rand (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//--------------------------------------------------------------------------
 
 var http = require('http');
 var server = http.createServer(app);
@@ -72,6 +87,7 @@ server.listen(8080);
 var io = require('socket.io').listen(server);
 
 var connections = {};
+var connectionsfull = {};
 
 function getName (connections, socket) {
     var name;
@@ -91,17 +107,112 @@ io.sockets.on('connection', function (socket) {
 	//Chat from C to C
 	socket.on('chatctoc', function (data) {
 		// und an mich selbst, wieder zurück das ich ihn auch sehe
-		var name = getName(connections, socket);
+		var name = getName(connections, socket.id);
 		console.log('Der name ist:' + name);
 		io.sockets.emit('awchatctoc', { zeit: new Date(), text: data.text,name: name});
 		console.log(new Date() + ' from:' + name + ' text:' + data.text);
 	});
 	
 	socket.on('join', function(data) {
-        connections[data.name] = socket;
-		io.sockets.emit('awchatctoc', { zeit: new Date(), text: data.name + ' ist dem chat beigetreten',name: 'SYSTEM'});
-		console.log(new Date() +""+ data.name + "ist dem chat beigetreten SYSTEM");
+		if(data.key == idcheck.get(data.name)){
+			connections[data.name] = socket.id;
+			connectionsfull[data.name] = socket;
+			socket.emit('waitstart', {name: data.name,groupsize:groupsize});
+			var temp = new Map();
+			temp = groupGelb.get(nametoGroup.get(data.name));
+			if(typeof temp === "undefined"){
+				temp = new Map();
+				temp.set(socket.id,socket.id);
+				groupGelb.set(nametoGroup.get(data.name),temp);
+			}else{
+				temp.set(socket.id,socket.id);
+			}
+		}else{
+			console.log('da hat jemand was an der URL geaendert --> logout');
+			socket.emit('logout', {});
+		}
+		//io.sockets.emit('awchatctoc', { zeit: new Date(), text: data.name + ' ist dem chat beigetreten',name: 'SYSTEM'});
+		//console.log(new Date() +""+ data.name + "ist dem chat beigetreten SYSTEM");
     });
+	
+	socket.on('joinstatus', function(data) {
+		var gruen = 0;
+		var gelb = 0;
+		
+		var idSocketid = new Map();
+		idSocketid = groupGelb.get(nametoGroup.get(data.name));
+		if(typeof idSocketid !== "undefined"){
+				idSocketid.forEach(function(value, key) {			
+					if(typeof io.sockets.connected[value] === "undefined"){
+						//nicht senden, Player entfernen und Admin melden
+						console.log('\u001b[31m check PlayerID '+ key + ' verloren \u001b[0m');
+						if (debug) console.log("\007");
+						//entfernen der ID über PlayerID
+						if(idSocketid.has(key))idSocketid.delete(key);									
+					}
+				});
+			gelb = idSocketid.size;
+		}
+		
+		idSocketid = groupGruen.get(nametoGroup.get(data.name));
+		if(typeof idSocketid !== "undefined"){
+			idSocketid.forEach(function(value, key) {			
+				if(typeof io.sockets.connected[value] === "undefined"){
+					//nicht senden, Player entfernen und Admin melden
+					console.log('\u001b[31m check PlayerID '+ key + ' verloren \u001b[0m');
+					if (debug) console.log("\007");
+					//key wieder freigeben
+							
+					//entfernen der ID über PlayerID
+					if(idSocketid.has(key))idSocketid.delete(key);									
+				}
+			});
+			gruen = idSocketid.size;
+		}
+		if(gruen == groupsize){
+			socket.emit('startchat', {});
+		}else{
+			console.log('gruen '+gruen+'  gelb'+ gelb);
+			socket.emit('rejoinstatus', {gruen: gruen,gelb:gelb,groupsize:groupsize});
+		}
+	});
+	
+	socket.on('joinstatusgruen', function(data) {
+		var idSocketid = new Map();
+		idSocketid = groupGelb.get(nametoGroup.get(data.name));
+		if(typeof idSocketid !== "undefined"){
+			idSocketid = groupGelb.get(nametoGroup.get(data.name));
+			idSocketid.delete(socket.id);
+		}
+		
+		var temp = new Map();
+		temp = groupGruen.get(nametoGroup.get(data.name));
+		if(typeof temp === "undefined"){
+			temp = new Map();
+			temp.set(socket.id,socket.id);
+			groupGruen.set(nametoGroup.get(data.name),temp);
+		}else{
+			temp.set(socket.id,socket.id);
+		}
+	});
+	socket.on('joinstatusgelb', function(data) {
+		var idSocketid = new Map();
+		idSocketid = groupGruen.get(nametoGroup.get(data.name));
+		if(typeof idSocketid !== "undefined"){
+			idSocketid = groupGruen.get(nametoGroup.get(data.name));
+			idSocketid.delete(socket.id);
+		}
+		
+		var temp = new Map();
+		temp = groupGelb.get(nametoGroup.get(data.name));
+		if(typeof temp === "undefined"){
+			temp = new Map();
+			temp.set(socket.id,socket.id);
+			groupGelb.set(nametoGroup.get(data.name),temp);
+		}else{
+			temp.set(socket.id,socket.id);
+		}
+	});
 	
 });
 
